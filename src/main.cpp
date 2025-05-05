522a0c1ef49f4ecb043758e476f877a8c8bac9d8
WEARESTIMULAR <stimular.github@gmail.com>
2025-05-05 16:59:30 +0200

    deleted commented out code

--- src/main.cpp
+++ src/main.cpp
@@ -3,50 +3,9 @@
 
 int main(){
 
-    //std::ifstream file;
-    //std::string line;
-    //file.open ("../src/shaders/fragment.frag");
-    //while(std::getline(file, line)){
-    //    std::cout << line << std::endl;
-    //}
-
-
-
-
-    //till here in window
-
-    //not yet
-    //glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
-
-    //make shader
-    //unsigned int shader = make_shader(
-    //    "../src/shaders/vertex.vert",
-    //    "../src/shaders/fragment.frag"
-    //);
-
-
-    //TriangleMesh* pTriangle = new TriangleMesh();
-
-    // loop already in rederer
-    //while (!glfwWindowShouldClose(window)){
-    //    glfwPollEvents();
-
-
-    //    glClear(GL_COLOR_BUFFER_BIT);
-    //    glUseProgram(shader);
-    //    pTriangle->Draw();
-    //    glfwSwapBuffers(window);
-    //}
-
-    //till here
-
-    //glDeleteShader(shader);
-    //glfwTerminate();
-
     Engine engine;
     engine.Init();
     engine.Run();
-
 
     return 0;
 }
